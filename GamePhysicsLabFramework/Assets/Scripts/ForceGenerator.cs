6e25181cb8b1be499b39b7a082d6d63b78268b2e
Nat <nathanialwalker@mymail.champlain.edu>
2019-09-12 10:58:44 -0400

    Lab 2 stuff

---
+++ GamePhysicsLabFramework/Assets/Scripts/ForceGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceGenerator
+{
+    public static Vector2 GenerateForce_Gravity(float particleMass, float gravitationalConstant, Vector2 worldUp)
+    {
+        // f = mg
+        Vector2 f_gravity = particleMass * gravitationalConstant * worldUp;
+        return f_gravity;
+    }
+    public static Vector2 GenerateForce_Normal(Vector2 f_gravity, Vector2 surfaceNormal_unit)
+    {
+        // f_normal = proj(f_gravity, surfaceNormal_unit)
+        Vector2 f_normal = Vector3.Project(new Vector3(f_gravity.x, f_gravity.y, 1.0f), new Vector3(surfaceNormal_unit.x, surfaceNormal_unit.y, 1.0f));
+        return f_normal;
+    }
+    public static Vector2 GenerateForce_Sliding(Vector2 f_gravity, Vector2 f_normal)
+    {
+        //f_sliding = f_gravity + f_normal
+        Vector2 f_sliding = f_gravity + f_normal;
+        return f_sliding;
+    }
+    public static Vector2 GenerateForce_Friction_Static(Vector2 f_normal, Vector2 f_opposing, float frictionCoefficient_static)
+    {
+        // f_friction_s = -f_opposing if less than max, else -coeff*f_normal
+        // (max amount is coeff*|f_normal|)
+        Vector2 f_friction_s = -f_opposing < (frictionCoefficient_static * new Vector2(Mathf.Abs(f_normal.x), Mathf.Abs(f_normal.y))) ? -(frictionCoefficient_static)*f_normal : new Vector2(0.0f, 0.0f);
+        return f_friction_s;
+    }
+}
---
+++ GamePhysicsLabFramework/Assets/Scripts/Particle2D.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle2D : MonoBehaviour
+{
+    // Step 1-1
+    public Vector2 position, velocity, acceleration;
+    public float rotation, angularVelocity, angularAcceleration;
+
+    // Step 2-1
+    public float startingMass;
+    float mass, massInv;
+    public void SetMass(float newMass)
+    {
+        //mass = newMass > 0.0f ? newMass : 0.0f;
+        mass = Mathf.Max(0.0f, newMass);
+        massInv = mass > 0.0f ? 1.0f / mass : 0.0f;
+    }
+    public float GetMass()
+    {
+        return mass;
+    }
+
+    // Step 2-2
+    Vector2 force;
+    public void AddForce(Vector2 newForce)
+    {
+        // D'Alembert
+        force += newForce;
+    }
+    void UpdateAcceleration()
+    {
+        // Newton2
+        acceleration = massInv * force;
+
+        force.Set(0.0f, 0.0f);
+    }
+
+    // Step 1-2
+    void updatePositionExplicitEuler(float dt)
+    {
+        // x(t+dt) = x(t) + v(t)dt
+        // Euler's method:
+        // F(t+dt) = F(t) + f(t)dt
+        //                + (dF/dt)dt
+        position += velocity * dt;
+
+        // v(t+dt) = v(t) + a(t)dt
+        velocity += acceleration * dt;
+    }
+    void updatePositionKinematic(float dt)
+    {
+        // x(t+dt) = x(t) + v(t)dt + 1/2a(t)dt^2
+        position += velocity * dt + (0.5f * acceleration * (dt * dt));
+
+        velocity += acceleration * dt;
+    }
+    void updateRotationEulerExplicit(float dt)
+    {
+        rotation += angularVelocity * dt;
+        angularVelocity += angularAcceleration * dt;
+    }
+    void updateRotationKinematic(float dt)
+    {
+        rotation += angularVelocity * dt + (0.5f * angularAcceleration * (dt * dt));
+        angularVelocity += angularAcceleration * dt;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetMass(startingMass);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Step 1-3
+        // Integrate
+        updatePositionExplicitEuler(Time.fixedDeltaTime);
+        //updatePositionKinematic(Time.fixedDeltaTime);
+        //updateRotationEulerExplicit(Time.fixedDeltaTime);
+        //updateRotationKinematic(Time.fixedDeltaTime);
+
+        // Step 2-2
+        UpdateAcceleration();
+
+        // Apply to transform
+        transform.position = position;
+        // transform.Rotate(0, 0, rotation);
+
+        // Step 1-4
+        // test
+        // acceleration.x = -Mathf.Sin(Time.fixedTime);
+        // angularAcceleration = -Mathf.Sin(Time.fixedTime);
+
+        // Step 2-2
+        // f_gravity: f = mg
+        //Vector2 f_gravity = mass * new Vector2(0.0f, -9.8f);
+        //AddForce(f_gravity);
+        AddForce(ForceGenerator.GenerateForce_Gravity(mass, -9.8f, Vector2.up));
+    }
+}
