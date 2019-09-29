d09f50b3da7a9e1a648e15d7792971bbe2bef096
Nat <nathanialwalker@mymail.champlain.edu>
2019-09-29 14:39:50 -0400

    Please

--- GamePhysicsLabFramework/Assets/Scripts/Collisions/AxisAlignedBoundingBox2D.cs
+++ GamePhysicsLabFramework/Assets/Scripts/Collisions/AxisAlignedBoundingBox2D.cs
@@ -20,14 +20,30 @@ public class AxisAlignedBoundingBox2D : CollisionHull2D
 
     public override bool TestCollisionVsCircle(CircleCollisionHull2D other)
     {
-        // pass if distance between centers <= sum of radii
-        // optimized collision passes if (distance between centers) squared <= (sum of radii) sq
-        // 1. get the two centers
-        // 2. difference between centers
-        // 3. distance squared = dot(diff, diff) (Vector.squaremagnitude)
-        // 4. sum of radii
-        // 5. square sum
-        // 6. DO THE TEST: distSq <= sumSq
+        // calculate closest point by clamping circle center on each dimension
+        // passes if closest point vs circle passes
+        // 1. get the radius of the circle
+        // 2. get the ymin, ymax
+        // 3. get the xmin, xmax
+        // 4. clamp the circle to the box so that we are able to get proper collision
+        // 5. max0 >= radius
+        // 6. radius >= min0
+
+        float radius = other.radius;
+
+        float minY = this.transform.position.y - (this.transform.localScale.y * 0.5f);
+        float maxY = this.transform.position.y + (this.transform.localScale.y * 0.5f);
+
+        float minX = this.transform.position.x - (this.transform.localScale.x * 0.5f);
+        float maxX = this.transform.position.x + (this.transform.localScale.x * 0.5f);
+
+        float nearestX = other.transform.position.x * Mathf.Clamp(radius, minX, maxX);
+        float nearestY = other.transform.position.y * Mathf.Clamp(radius, minY, maxY);
+
+        if (nearestX >= radius && radius >= nearestY)
+        {
+            return true;
+        }
 
         return false;
     }
@@ -35,8 +51,28 @@ public class AxisAlignedBoundingBox2D : CollisionHull2D
     public override bool TestCollisionVsAABB(AxisAlignedBoundingBox2D other)
     {
         // for each dimension, max extent of A greater than min extent of B
-        // 1. .....
+        // 1. get the ymin, ymax of both boxes
+        // 2. get the xmin, xmax of both boxes
+        // 3. max of box 1 >= min of box 2
+        // 4. max of box 2 >= min of box 1
 
+        float minY1 = this.transform.position.y - (this.transform.localScale.y * 0.5f);
+        float maxY1 = this.transform.position.y + (this.transform.localScale.y * 0.5f);
+        float minX1 = this.transform.position.x - (this.transform.localScale.x * 0.5f);
+        float maxX1 = this.transform.position.x + (this.transform.localScale.x * 0.5f);
+
+        float minY2 = other.transform.position.y - (other.transform.localScale.y * 0.5f);
+        float maxY2 = other.transform.position.y + (other.transform.localScale.y * 0.5f);
+        float minX2 = other.transform.position.x - (other.transform.localScale.x * 0.5f);
+        float maxX2 = other.transform.position.x + (other.transform.localScale.x * 0.5f);
+
+        if (maxX1 >= minX2 && maxY1 >= minY2)
+        {
+            if (maxX2 >= minX1 && maxY2 >= minY1)
+            {
+                return true;
+            }
+        }
 
         return false;
     }
@@ -45,8 +81,26 @@ public class AxisAlignedBoundingBox2D : CollisionHull2D
     {
         // same as above twice
         // first, test AABB vs max extents
-        //then, multiply by OBB inverse matrix, do text again
-        // 1. .....
+        // then, multiply by OBB inverse matrix, do test again
+        // 1. get the ymin, ymax, xmin, xmax of both boxes
+        // 2. max of box 1 >= min of box 2
+        // 3. max of box 2 >= min of box 1
+        // 4. create OBB inverse matrix
+        // 5. multiply inverse matrix by max extents
+        // 6. max of box 1 >= min of box 2
+        // 7. max of box 2 >= min of box 1
+
+        float minY1 = this.transform.position.y - (this.transform.localScale.y * 0.5f);
+        float maxY1 = this.transform.position.y + (this.transform.localScale.y * 0.5f);
+        float minX1 = this.transform.position.x - (this.transform.localScale.x * 0.5f);
+        float maxX1 = this.transform.position.x + (this.transform.localScale.x * 0.5f);
+
+        float minY2 = other.transform.position.y - (other.transform.localScale.y * 0.5f);
+        float maxY2 = other.transform.position.y + (other.transform.localScale.y * 0.5f);
+        float minX2 = other.transform.position.x - (other.transform.localScale.x * 0.5f);
+        float maxX2 = other.transform.position.x + (other.transform.localScale.x * 0.5f);
+
+        
 
         return false;
     }
