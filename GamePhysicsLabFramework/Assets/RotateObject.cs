4ae4900b5d189bacfac60a133ac837e416cf1907
kelly <kiherstine@gmail.com>
2019-12-06 19:37:53 -0500

    edited RotateObject script

--- GamePhysicsLabFramework/Assets/RotateObject.cs
+++ GamePhysicsLabFramework/Assets/RotateObject.cs
@@ -4,26 +4,45 @@ using UnityEngine;
 
 public class RotateObject : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector3 prevPosition;
+    Vector3 prevDelta;
     void Start()
     {
-        
+        prevPosition = Vector3.zero;
+        prevDelta = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
-        
-        Quaternion targetRotation = Quaternion.LookRotation(mousePos - transform.position);
-        
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 2.0f * Time.deltaTime);
 
-        //Vector3 position = Camera.main.WorldToScreenPoint(transform.position);
-        //Vector3 direction = Input.mousePosition - position;
-        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-      
+        if (Mathf.Abs(Input.GetAxis("Vertical")) > 0)
+        {
+            //Set a rate at which we should turn
+            float turnSpeed = 20 * Time.deltaTime;
+            //Connect turning rate to horizonal motion for smooth transition
+            float rotate = Input.GetAxis("Vertical") * turnSpeed;
+            //            //Get current rotation
+            //            float currentRotation = gameObject.transform.rotation.z;
+            //            //Add current rotation to rotation rate to get new rotation
+            //            Quaternion rotation = Quaternion.Euler (0, 0, currentRotation + rotate);
+            //            //Move object to new rotation
+            //            gameObject.transform.rotation = rotation;
+            gameObject.transform.Rotate(Vector3.forward * rotate);
+        }
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0)
+        {
+            //Set a rate at which we should turn
+            float turnSpeed = 20 * Time.deltaTime;
+            //Connect turning rate to horizonal motion for smooth transition
+            float rotate = Input.GetAxis("Horizontal") * turnSpeed;
+            //            //Get current rotation
+            //            float currentRotation = gameObject.transform.rotation.z;
+            //            //Add current rotation to rotation rate to get new rotation
+            //            Quaternion rotation = Quaternion.Euler (0, 0, currentRotation + rotate);
+            //            //Move object to new rotation
+            //            gameObject.transform.rotation = rotation;
+            gameObject.transform.Rotate(rotate, 0, 0);
+        }
     }
 }
